f56aa5596953f0579d9934b34b967ab64729e5d7
MHD-Noor <noor4apps@gmail.com>
2025-03-27 01:06:42 +0300

    Added HasFactory trait to models to enable factory usage

--- app/Models/PriceList.php
+++ app/Models/PriceList.php
@@ -2,11 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class PriceList extends Model
 {
+    use HasFactory;
+
     protected $fillable = ['product_id', 'country_code', 'country_id', 'currency_code', 'currency_id', 'price', 'starts_at', 'ends_at', 'priority'];
 
     public function product(): BelongsTo
